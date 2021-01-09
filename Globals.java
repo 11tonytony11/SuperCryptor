eb9ea80a0a7c3ce41ace5638fda889994dfe6df4
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2021-01-09 18:18:48 +0200

    Started working on Globals and constants file. More vars will be added soon

---
+++ Globals.java
@@ -0,0 +1,5 @@
+public class Globals
+{
+    public static final String[] Encryptions = { "Byte Shuffle Card", "Bit Shuffle Card", "Byte Rev", "Byte Xor", "Bit Flip"};
+    public static final String[] Generators = { "Fibonacci", "Pascal", "Hanoi", "Pseudo-Random", "Random" };
+}
