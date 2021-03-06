13d62c95b553efbb9029f51a768f913eb4f9021d
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2021-03-06 19:33:53 +0200

    Added more global values for cleaner code

--- Globals.java
+++ Globals.java
@@ -1,5 +1,25 @@
 public class Globals
 {
-    public static final String[] Encryptions = { "Byte Shuffle Card", "Bit Shuffle Card", "Byte Rev", "Byte Xor", "Bit Flip"};
-    public static final String[] Generators = { "Fibonacci", "Pascal", "Hanoi", "Pseudo-Random", "Random" };
+    public static final String[] Encryptions = { "Path Encryption", "Knight's tour", "Sudoku", "Shuffle Card" , "Half XOR"  };
+    public static final String[] Generators  = { "Fibonacci", "Pascal", "Hanoi" };
+
+    public static String REGEX_FILTER = "[^a-zA-Z0-9,]";
+    public static String KEY_PARSER = ",";
+
+    public static final int CHESS_BOARD  = 5;
+    public static final int SUDOKU_BOARD = 9;
+
+    public static int FIELD_LEN = 20;
+
+    public static final int[][] SUDOKU = new int[][] {
+            { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+            { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+            { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+            { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+            { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+            { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+            { 0, 0, 5, 2, 0, 6, 3, 0, 0 }
+    };
 }
