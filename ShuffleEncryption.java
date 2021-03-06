fee9b6198911cd26be25e0cfcdc8257dab18ef06
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2021-03-06 19:37:58 +0200

    Changed algorithm and refactored

--- ShuffleEncryption.java
+++ ShuffleEncryption.java
@@ -2,42 +2,45 @@ import java.util.List;
 
 public class ShuffleEncryption extends Encryption
 {
-
+    //-----------------------------------------------------------------------------------------------------------
+    /*
+    This is the encryption constructor
+    Input:  Key series, file binary
+    Output: None
+    */
     public ShuffleEncryption(List<Integer> newKeys, byte[] newBin)
     {
         super(newKeys, newBin);
     }
-
-    @Override
+    //-----------------------------------------------------------------------------------------------------------
+    /*
+    This function is the encryption function
+    Input:  None
+    Output: encrypted binary
+    */
     public byte[] encrypt()
     {
-        int key = this.key.get(0) % this.bin.length;
+        int key = this.key.get(0) % Byte.SIZE;
 
-        while (key-- > 0)
+        if(key == 4)
+            key = 3;
+
+        for(int i = 0; i < this.bin.length; i++)
         {
-            byte tmp = this.bin[0];
-            for (int i = 1; i < this.bin.length; i++)
-                this.bin[i - 1] = this.bin[i];
-            this.bin[this.bin.length - 1] = tmp;
+            this.bin[i] = (byte) (this.bin[i] ^ 1 << key);
+            this.bin[i] = (byte) (this.bin[i] ^ 1 << (Byte.SIZE - key));
         }
 
         return this.bin;
     }
-
-    @Override
+    //-----------------------------------------------------------------------------------------------------------
+    /*
+    This function is the decryption function
+    Input:  None
+    Output: decrypted binary
+    */
     public byte[] decrypt()
     {
-        int key = this.key.get(0) % this.bin.length;
-        key = this.bin.length - key;
-
-        while (key-- > 0)
-        {
-            byte tmp = this.bin[0];
-            for (int i = 1; i < this.bin.length; i++)
-                this.bin[i - 1] = this.bin[i];
-            this.bin[this.bin.length - 1] = tmp;
-        }
-
-        return this.bin;
+        return encrypt();
     }
 }
