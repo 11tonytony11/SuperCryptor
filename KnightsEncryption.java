710fab5372e13a266b53d8875331166d206506dc
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2021-03-08 13:25:57 +0200

    Refactoring and documentation added

--- KnightsEncryption.java
+++ KnightsEncryption.java
@@ -2,16 +2,26 @@ import java.util.List;
 
 public class KnightsEncryption extends Encryption
 {
-
+    //-----------------------------------------------------------------------------------------------------------
+    /*
+    This is the encryption constructor
+    Input:  Key series, file binary
+    Output: None
+    */
     public KnightsEncryption(List<Integer> newKeys, byte[] newBin)
     {
         super(newKeys, newBin);
     }
-
-    @Override
+    //-----------------------------------------------------------------------------------------------------------
+    /*
+    This function is the encryption function
+    Input:  None
+    Output: encrypted binary
+    */
     public byte[] encrypt()
     {
-        int key = 0;
+        int key;
+
         for(int i = 0; i < this.bin.length; i++)
         {
             key = this.key.get(i % this.key.size());
@@ -20,8 +30,12 @@ public class KnightsEncryption extends Encryption
 
         return this.bin;
     }
-
-    @Override
+    //-----------------------------------------------------------------------------------------------------------
+    /*
+    This function is the decryption function
+    Input:  None
+    Output: decrypted binary
+    */
     public byte[] decrypt()
     {
         return encrypt();
